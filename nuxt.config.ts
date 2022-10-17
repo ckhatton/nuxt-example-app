17c9f7eb3167e9117a031f8ed69e298ce4acc2b1
Chris Hatton <mail@ckhatton.com>
2022-10-17 18:18:01 +0100

    build(config): update head details

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,19 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { name: 'robots', content: 'noindex' },
-      { name: 'googlebot', content: 'noindex' }
-    ],
-    link: [
+  app: {
+    head: {
+      title: process.env.npm_package_name || '',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { name: 'description', content: process.env.npm_package_description || '' },
+        { name: 'robots', content: 'noindex' },
+        { name: 'googlebot', content: 'noindex' }
+      ],
+      link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      ]
+    }
   },
   serverMiddleware: [
     '~/api/calculate'
