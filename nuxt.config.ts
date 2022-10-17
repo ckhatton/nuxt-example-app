2a8f33b9453ec65aac928a7815480f4f565a435a
Chris Hatton <mail@ckhatton.com>
2022-10-17 12:21:05 +0100

    build(config): add head details

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'robots', content: 'noindex' },
+      { name: 'googlebot', content: 'noindex' }
+    ],
+    link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
   modules: [
     // https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
