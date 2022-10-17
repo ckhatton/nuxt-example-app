7277741a3a5db8a2c2085bbeaf29ddd3f1b9632b
Chris Hatton <mail@ckhatton.com>
2022-10-17 08:50:26 +0100

    feat(package): eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  modules: [
+    // https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
   typescript: {
     shim: false
   }
