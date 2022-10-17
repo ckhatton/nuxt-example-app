3e447f117b15750021c44a7c617d452817978182
Chris Hatton <mail@ckhatton.com>
2022-10-17 11:38:18 +0100

    feat(package): jest re-setup

--- tests/components/header.spec.js
+++ tests/components/header.spec.js
@@ -1,11 +1,7 @@
-import { setup, mount } from '@nuxt/test-utils-edge';
+import { mount } from '@vue/test-utils';
 import Header from '@/components/Header.vue';
 
 describe('Header component tests: ', async () => {
-  await setup({
-    // test context options
-  });
-
   test('It is a Vue component', () => {
     const wrapper = mount(Header);
     expect(wrapper.isVueInstance()).toBeTruthy();
