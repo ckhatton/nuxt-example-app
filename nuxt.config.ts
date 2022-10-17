e409aac4333757dbad2995223b939f1685c4d5b7
Chris Hatton <mail@ckhatton.com>
2022-10-17 11:53:22 +0100

    feat(package): tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@
 export default defineNuxtConfig({
   modules: [
     // https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    // https://tailwindcss.nuxtjs.org
+    '@nuxtjs/tailwindcss'
   ],
   typescript: {
     shim: false
