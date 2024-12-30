59a4a07ec7dd57d46eb406a51be4dacc524c90cc
Chris Hatton <mail@ckhatton.com>
2024-12-30 12:52:47 +0000

    build(test): upgrade

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,19 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.cjs.js',
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue3-jest',
+    '^.+\\.ts?$': 'ts-jest',
+    '.*\\.(vue)$': '@vue/vue3-jest',
   },
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue',
   ],
-}
+  testEnvironmentOptions: {
+    customExportConditions: ["node", "node-addons"],
+  },
+};
