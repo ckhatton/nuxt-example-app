125e1e55096bf1a15d78cb1ceff35e7f68e6ac25
Chris Hatton <mail@ckhatton.com>
2024-12-30 03:17:52 +0000

    build(contact): add further information

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="md:flex bg-white rounded-lg shadow-lg p-6 mb-4">
-    <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" src="/avatar.png" loading="lazy" />
+    <img class="h-16 w-16 md:h-24 md:w-24 rounded-full bg-gradient mx-auto md:mx-0 md:mr-6" src="/avatar.png" />
     <div class="text-center md:text-left">
       <h3 class="text-lg">Christopher Hatton</h3>
-      <div class="text-green font-semibold">Web Developer: Full Stack</div>
+      <div class="text-green font-semibold">Software Engineer: Fullstack</div>
       <div class="text-gray-700"><a href="mailto:mail@ckhatton.com">mail@ckhatton.com</a></div>
-      <div class="text-gray-700"><a href="https://ckhatton.com">ckhatton.com</a></div>
+      <div class="text-gray-700"><a href="tel:+447961711210">+44 (0) 7961 711 210</a></div>
+      <div class="text-gray-700">United Kingdom</div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Contact'
+<style scoped>
+.bg-gradient {
+  background-image: radial-gradient(circle closest-side, #ffffff, #225378);
 }
-</script>
+</style>
