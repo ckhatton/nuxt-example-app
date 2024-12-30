6adddb0a237abfd726aac35091f3b55753bc1266
Chris Hatton <mail@ckhatton.com>
2024-12-30 03:08:00 +0000

    build(header): change title

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-blue mb-4 p-8 rounded-lg shadow-lg font-extrabold text-white tracking-wide flex flex-wrap justify-between items-center">
-    <h2 class="text-2xl">Christopher Hatton</h2>
+    <h2 class="text-2xl">Nuxt v3 Example App</h2>
   </div>
 </template>
 
